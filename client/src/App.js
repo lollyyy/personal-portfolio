2c311ab67f1688eaa3c65504c57e42bc3e2c4054
Jaakko <jaakko.ihanamaki@outlook.com>
2019-05-04 23:38:26 +0300

    Add Github link to index.html and add styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ const App = () => {
     <div>
     <h1>Hello World!</h1>
     <p>This is Jaakko's personal portfolio</p>
+
+    <p>While you're waiting for this site to finish, you can follow the progress
+    on Github:</p>
+    <a href="https://github.com/lollyyy/personal-portfolio">
+    <img src="../public/assets/github.png" alt="Link to this page's github repo" />
+    </a>
     </div>
   )
 }
