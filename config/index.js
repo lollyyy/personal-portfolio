6699ef70470f1cf296f87f50d69db59654357d8c
Jaakko <jaakko.ihanamaki@outlook.com>
2019-05-04 23:20:09 +0300

    FIXED Express server

--- config/index.js
+++ config/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
  });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(port, function() {
+  console.log('App listening on ', port)
+});
