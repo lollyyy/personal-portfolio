b9076ec0f59c7b1031b67a4645cca7d65b8bd143
Jaakko <jaakko.ihanamaki@outlook.com>
2019-05-04 22:56:05 +0300

    Fix bugs in Express server

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 
